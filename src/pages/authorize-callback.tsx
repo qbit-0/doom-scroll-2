a10ced3797097d2ac87ee48b607ad490b1321b33
Duy Pham <duypham12241999@gmail.com>
2022-08-21 14:04:42 -0700

    run prettier

--- src/pages/authorize-callback.tsx
+++ src/pages/authorize-callback.tsx
@@ -5,8 +5,8 @@ import { FC, useEffect } from "react";
 import { mutate } from "swr";
 
 import PageFrame from "../components/PageFrame";
-import { redditApi } from "../lib/reddit/redditServerApi";
 import { getUserAccessToken } from "../lib/reddit/redditOAuth";
+import { redditApi } from "../lib/reddit/redditServerApi";
 import { withSessionSsr } from "../lib/session/withSession";
 
 export const getServerSideProps: GetServerSideProps = withSessionSsr(
