530006d0f26b5cced410a1d259af7f41342d229a
Duy Pham <duypham12241999@gmail.com>
2022-10-28 17:23:04 -0700

    Update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,19 +10,37 @@ import theme from "../styles/theme";
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-    <Head>
-      <title>DoomScroll</title>
-    </Head>
-    <ChakraProvider colorModeManager={localStorageManager} theme={theme}>
-      <DisplaySettingsProvider>
-        <PostsFilterProvider>
-          <CommentsFilterProvider>
-            <Component {...pageProps} />
-          </CommentsFilterProvider>
-        </PostsFilterProvider>
-      </DisplaySettingsProvider>
-    </ChakraProvider>
-</>
+      <Head>
+        <title>DoomScroll</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <ChakraProvider colorModeManager={localStorageManager} theme={theme}>
+        <DisplaySettingsProvider>
+          <PostsFilterProvider>
+            <CommentsFilterProvider>
+              <Component {...pageProps} />
+            </CommentsFilterProvider>
+          </PostsFilterProvider>
+        </DisplaySettingsProvider>
+      </ChakraProvider>
+    </>
   );
 };
 
